fc197923b1e1959ea43788df833226210c873fc7
Sergio <chechucarrasco@gmail.com>
2020-10-29 22:21:32 +0100

    solution error in port config

--- server/server.js
+++ server/server.js
@@ -46,6 +46,6 @@ app.delete('/usuario', function(req, res) {
     res.json('delete usuario');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
 });
