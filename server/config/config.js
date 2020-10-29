5b9b8378e1bf061b7f8f82c80dc4c2aff2581921
Sergio <chechucarrasco@gmail.com>
2020-10-29 17:57:19 +0100

    Initial commit

---
+++ server/config/config.js
@@ -0,0 +1,2 @@
+//Port
+process.env.PORT = process.env.PORT || 3000;
---
+++ server/server.js
@@ -0,0 +1,51 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get('/usuario', function(req, res) {
+    res.json('get usuario');
+});
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+
+    } else {
+
+        res.json({
+            persona: body
+        });
+    }
+
+});
+
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario');
+});
+
+app.listen(3000, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
