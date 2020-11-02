7a18d75c95c75982e2af9ffee7e4256fc3e9d4a3
Sergio <chechucarrasco@gmail.com>
2020-11-02 18:01:45 +0100

    mongoose-unique-validator installation and validations of the 'email' and 'role' fields

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -1,4 +1,13 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+//Forma de definir la lista de roles válidos y el mensaje de error que se devolverá. {VALUE} hará referencia
+//a lo que el usuario haya introducido, si introduce en el campo rol un valor diferente a ADMIN_ROLE o USER_ROLE
+//devolverá ese error. rolesValidos debe hacer referencia a la propiedad 'enum' de roles en el esquema
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol válido'
+};
 
 let Schema = mongoose.Schema;
 
@@ -10,6 +19,7 @@ let usuarioSchema = new Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: [true, 'El correo es necesario']
     },
     password: {
@@ -22,7 +32,8 @@ let usuarioSchema = new Schema({
     },
     role: {
         type: String,
-        default: 'USER_ROLE'
+        default: 'USER_ROLE',
+        enum: rolesValidos
     },
     estado: {
         type: Boolean,
@@ -35,6 +46,10 @@ let usuarioSchema = new Schema({
         default: false
     }
 });
+//Aquí decimos a este esquema que use el plugin mongoose-unique-validator. En {PATH} mongoose inyectará el elemento
+//que esté marcado en el esquema como 'unique: true' y falte para mostrarlo en el mensaje de error. Es decir si
+//se repite un elemento que ya está introducido devolverá ese error. Ej: 'email debe ser único' ó 'dni debe ser único'
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
 
 //Exportamos el esquema con el nombre de Usuario, que tendrá la configuración de 'usuarioSchema'
 module.exports = mongoose.model('Usuario', usuarioSchema);
