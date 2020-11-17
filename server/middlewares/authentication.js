37bda4e101da6728baa22fd347ed60d08831f53d
Sergio <chechucarrasco@gmail.com>
2020-11-17 10:54:38 +0100

    Authentication.js comments

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -1,28 +1,19 @@
-//Librería jsonwebtoken
+// jsonwebtoken library
 const jwt = require('jsonwebtoken');
 
 
 
 //=================================================================
-//  Verificar token
+//  Token verify
 //=================================================================
+const verifyToken = (req, res, next) => {
 
-//El next lo que hará es continuar con la ejecución del programa
-let verifyToken = (req, res, next) => {
-
-    //Con req.get('nombre-header') obtenemos el header que hemos enviado en la petición. Este es un header 
-    //personalizado al que hemos llamado token, podriamos llamarle authorization,autentication o como quisiéramos
     let token = req.get('token');
 
-    //.verify() función de la librería jsonwebtoken para verificar tokens, recibe el token, la semilla de autenticación
-    //y un callback que devuelve error o la información decodificada, el decoded será el 'payload' con la información
-    //que especificaramos en la creacion del token, es decir, la información del usuario(archivo login.js)
     jwt.verify(token, process.env.SEED, (err, decoded) => {
 
-        //Puede devolver un error porque el token no exista, esté expirado, la firma no sea correcta o cualquier
-        //otro error. 
         if (err) {
-            //Error 401 es un 'No autorization', error de autorización
+
             return res.status(401).json({
                 ok: false,
                 err: {
@@ -31,21 +22,16 @@ let verifyToken = (req, res, next) => {
             });
         }
 
-        //Si no ha habido error hacemos que cualquier petición pueda tener acceso a la información del usuario 
-        //después de haber pasado por este 'verifyToken'. Lo hacemos de la siguiente manera, en el objeto que 
-        //encriptamos en login.js viene el usuario, por eso lo recogemos así.
-        //Es decir, estamos colocando la información del usuario en la req como una nueva propiedad llamada usuario
+        // If everything has gone well, we take the data of the 'payload' and we put it in the req
         req.user = decoded.user;
-        //next() tiene que ir dentro de la función jwt.verify() porque si lo ponemos fuera se va a ejecutar
-        //siempre aunque el token no sea válido y siempre continuará la petición mostrando la información del
-        //usuario
+
         next();
     });
 
 }
 
 //=================================================================
-//  Verificar Role Admin
+//  'ROLE_ADMIN' Verify
 //=================================================================
 let verifyAdminRole = (req, res, next) => {
 
@@ -64,22 +50,16 @@ let verifyAdminRole = (req, res, next) => {
 }
 
 //=================================================================
-//  Verificar Token por URL para las imágenes
+//  Token verify by URL for images
 //=================================================================
 let verifyImgToken = (req, res, next) => {
 
-    //Recogemos el token por url como parámetro opcional
     let token = req.query.token;
 
-    //.verify() función de la librería jsonwebtoken para verificar tokens, recibe el token, la semilla de autenticación
-    //y un callback que devuelve error o la información decodificada, el decoded será el 'payload' con la información
-    //que especificaramos en la creacion del token, es decir, la información del usuario(archivo login.js)
     jwt.verify(token, process.env.SEED, (err, decoded) => {
 
-        //Puede devolver un error porque el token no exista, esté expirado, la firma no sea correcta o cualquier
-        //otro error. 
         if (err) {
-            //Error 401 es un 'No autorization', error de autorización
+
             return res.status(401).json({
                 ok: false,
                 err: {
@@ -88,14 +68,9 @@ let verifyImgToken = (req, res, next) => {
             });
         }
 
-        //Si no ha habido error hacemos que cualquier petición pueda tener acceso a la información del usuario 
-        //después de haber pasado por este 'verifyToken'. Lo hacemos de la siguiente manera, en el objeto que 
-        //encriptamos en login.js viene el usuario, por eso lo recogemos así.
-        //Es decir, estamos colocando la información del usuario en la req como una nueva propiedad llamada usuario
+        // If everything has gone well, we take the data of the 'payload' and we put it in the req
         req.user = decoded.user;
-        //next() tiene que ir dentro de la función jwt.verify() porque si lo ponemos fuera se va a ejecutar
-        //siempre aunque el token no sea válido y siempre continuará la petición mostrando la información del
-        //usuario
+
         next();
     });
 
